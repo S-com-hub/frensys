42a24410244f13e2c507ee55426b04915f58ae12
S-com-hub <72889632+S-com-hub@users.noreply.github.com>
2022-08-05 16:42:49 +0545

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,41 @@
+console.log("hello");   //print
+const name=birthday=>2000-birthday;
+console.log(name(1999));
+
+//alert("don't waste your time in this website");
+const day=prompt("enter the day name");
+switch(day) //switch
+{
+    case"monday":
+    console.log("today is chest day");
+    break;
+    case "tuesday":
+    console.log("today is back day");
+    break;
+    case "wednesday":
+    console.log("today is biceps day");
+    break;
+    default :
+    console.log("today is rest day");
+
+}
+const age=21;
+(age>=21 )? console.log("eligible for voting"):
+console.log("not elible for voting please wait untill you become adult");
+var i = 5;
+
+for (var i = 0; i < 10; i++) {
+  console.log("first DAY AS A INTERN");
+}
+const movements=[200,199,758,928,222];
+for(const movement of movements)
+{
+    console.log(movement);
+}
+console.log(test);   // undefined
+var test;
+test=10;
+console.log(test);
+
+
+// ajax call in javascript
